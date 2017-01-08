2130545005555f029ee8194108ba4627466ce22d
Jessie Pullaro <jessiepullaro@Jessies-MacBook-Air.local>
2017-01-08 17:14:55 -0500

    Fixed a server problem

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
+// viewed at http://localhost:8081
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(8081);
+
+console.log("Example app listening at http://localhost:8081");
