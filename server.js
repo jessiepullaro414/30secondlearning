4fa07681236ee2246c4061901045f88551d8f844
Jessie Pullaro <jessiepullaro@Jessies-MacBook-Air.local>
2017-01-10 19:09:06 -0500

    something to show gabe

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+//test to show
 // viewed at http://localhost:8081
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
